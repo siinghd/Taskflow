04bd92e2a61a6c5dc764b82cd58e9cf2b27fa615
Harpreet Singh <harpreetsingh@Harpreets-MacBook-Pro.local>
2023-06-15 21:07:42 +0200

    added backend and made some changes to workflow and docker files

--- frontend/app/[locale]/page.tsx
+++ frontend/app/[locale]/page.tsx
@@ -1,8 +1,10 @@
-import Image from 'next/image'
+import DialogLogin from '@/components/Dialog/DialogLogin';
 import {useTranslations} from 'next-intl';
+import { Button } from "@/components/ui/button"
 
 export default function Home() {
   const t = useTranslations('Index');
+
   return (
     <main className="mt-[100px] container">
       <div className='flex flex-col gap-8 pt-16 center'>
@@ -13,6 +15,11 @@ export default function Home() {
           {t('home-sub1')}<br/>
           <span className='font-bold'>{t('home-sub2')}</span>
         </p>
+        <div>
+          <DialogLogin 
+            triggerElement={<Button variant="outline">Enter</Button>}
+          />
+        </div>
       </div>
     </main>
   )
